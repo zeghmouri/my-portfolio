2a7881793542c99400783162cb7b276b3a336630
zeghmouri <hm_zeghmouri@esi.dz>
2022-03-16 15:12:31 +0100

    blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from '../styles/GlobalComponents'
+
+export default function Blog() {
+  return (
+    <div>
+        <h1>Coming Soon ...</h1>
+        <Link href="/">Back to Home page</Link>
+    </div>
+  )
+}
