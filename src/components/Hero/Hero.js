7badcad47bc581bbc80486b3adbd5c9c3b84d332
zeghmouri <hm_zeghmouri@esi.dz>
2022-04-07 14:08:42 +0100

    main section title

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Section, SectionText, SectionTitle, Link, avatarImg } from '../../styles/GlobalComponents';
+import { Section, SectionText, Link, avatarImg } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection , Image} from './HeroStyles';
+import { LeftSection , SectionTitle} from './HeroStyles';
 import Avatar from '@mui/material/Avatar';
 const Hero = (props) => (
   <>
     <Section row nopadding>
       <LeftSection>
-        <SectionTitle main center>
         <Avatar alt="my-avatar" src="/images/mypic.jpeg" sx={{ width: 100, height: 100 }}/>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hey there,
+        <SectionTitle main center>
+          Hey there,
         </SectionTitle>
         <SectionText>
           Hey there, This is ZEGHMOURI Mohamed Amine. I am a Full Stack Web developer ...
@@ -18,7 +18,6 @@ const Hero = (props) => (
           e.preventDefault();
           window.location.href = '/files/Resume Mohamed Amine ZEGHMOURI.pdf';
         }}>My Resume</Button>
-
       </LeftSection>
     </Section>
   </>
