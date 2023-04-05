484e6964c6903130a9e1be157d9f74fa52e78ac1
zeghmouri <ma.zeghmouri@gmail.com>
2023-04-05 11:45:38 +0100

    adding keywords

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>ZEGHMOURI - Full Stack Dev</title>
+        <meta name="keywords" content="zeghmouri, Mohamed Amine, zaki, developer, ESI, engineer, frontend, reactjs, projects, portfolio, Algeria, Setif"/>
       </Head>
       <Theme>
         <Component {...pageProps} />
