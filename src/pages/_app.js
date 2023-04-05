57fdb508c9acb21e8fb5232770f83915402a3c02
zeghmouri <ma.zeghmouri@gmail.com>
2023-04-05 11:53:16 +0100

    SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Theme from '../styles/theme';
 import Head from 'next/head'
+const Image = '/images/mypic.jpg'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,6 +8,15 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>ZEGHMOURI - Full Stack Dev</title>
         <meta name="keywords" content="zeghmouri, Mohamed Amine, zaki, developer, ESI, engineer, frontend, reactjs, projects, portfolio, Algeria, Setif"/>
+        <meta name="twitter:title" content="ZEGHMOURI - Full Stack Dev"/>
+        <meta property="og:title" content="ZEGHMOURI - Full Stack Dev"/>
+        <meta name="twitter:site" content="@zeghmouri"/>
+        <meta name="twitter:creator" content="@zeghmouri"/>
+        <meta property="og:site_name" content="Zeghmouri"/>
+        <meta property="og:type" content="website"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image" content={Image}/>
+        <meta property="og:image" content={Image}/>
       </Head>
       <Theme>
         <Component {...pageProps} />
